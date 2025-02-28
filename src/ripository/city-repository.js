4951249e32c8069046df3577216a4b9b0b4c959b
Saurabh Kushwaha <sk2005@gmail.com>
2025-02-28 18:56:05 +0530

    Completed city repository

--- src/ripository/city-repository.js
+++ src/ripository/city-repository.js
@@ -6,6 +6,7 @@ class Cityrepository{
             const city = await City.create({name});
             return city;
         }catch(error){
+            console.log("Something went wrong in the repository layer");
             throw{error};
         }
     }
@@ -17,7 +18,33 @@ class Cityrepository{
                     id:cityId
                 }
             });
+            return true;
         }catch(error){
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+
+    async updateCity(cityId,data){
+        try{
+            const city=await city.update(data,{
+                where:{
+                    id:cityId
+                }
+            });
+            return city;
+        }catch(error){
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+
+    }
+    async getCity(cityId){
+        try{
+            const city=await city.findByPk(cityId);
+            return city;
+        }catch(error){
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }
